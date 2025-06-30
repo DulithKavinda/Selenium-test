dde26d36192f7f1b606739406ece80f73cf671ac
DulithKavinda <121927516+DulithKavinda@users.noreply.github.com>
2025-06-30 12:10:14 +0530

    Add files via upload

---
+++ src/Test/java/part3_4/com/part3/javascript/JavaScriptTest.java
@@ -0,0 +1,11 @@
+package part3_4.com.part3.javascript;
+
+import org.testng.annotations.Test;
+import part3_4.com.base.BaseTest;
+
+public class JavaScriptTest extends BaseTest {
+    @Test
+    public void testScrollingToElement() {
+        homePage.goToForms();
+    }
+}
